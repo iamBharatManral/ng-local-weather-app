f71e0de71073242b0f6d98174a18e73a78a9ce52
iamBharatManral <justbharatmanral@gmail.com>
2023-06-26 17:44:59 +0530

    chore: add api keys for envs

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  appId: "67aaa64819df3f7f006925ae204ac630",
+  baseUrl: "https://"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  appId: "333115ddbab4f0d26a441df3180b7f53\n",
+  baseUrl: "https://"
+};
